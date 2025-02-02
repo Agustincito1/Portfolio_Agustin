420c37168f0ee5cd96c29892923f6a88a6470dfe
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-02 12:43:55 -0300

    estructurando la pagina con react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Header from '../components/header.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Header/>
+  </StrictMode>
+)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+//     <!-- Header pagina -->
 
-export default App
+
+//     <!-- Main de la pagina -->
+//     <main>
+//       <section>
+//         <!-- Introduccion -->
+//         <article id="Introduccion">
+//           <img src="assets/imgs/fotoDePerfil.jpeg" alt="Foto de perfil" />
+//           <nav id="Redes">
+//             <a><p>Github</p></a>
+//             <a><p>Gmail</p></a>
+//             <a><p>Linkedin</p></a>
+//             <i>CV</i>
+//           </nav>
+//         </article>
+//         <article>
+//           <h1>Agustin Lazaro Ardeola De Olivera</h1>
+//           <h2>Full stack</h2>
+//         </article>
+//         <article>
+//           <!-- Introduccion de quien soy -->
+//           <p>
+//             Persigo oportunidades que me permitan continuar ampliando mis
+//             habilidades y conocimientos como futuro Full Stack.
+//           </p>
+//         </article>
+//         <article>
+//           <!-- Lenguajes y frameworks que sé -->
+//           <ul>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//           </ul>
+//         </article>
+//         <article><h3>habilidades blandas</h3></article>
+//         <!-- Proyectos -->
+//         <article id="Proyectos">
+//           <h2>Projects</h2>
+//           <ul>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//           </ul>
+//         </article>
+//       </section>
+//     </main>
+
+//     <footer>
+//       <section>
+//         <!-- redireccion de página -->
+//         <article>
+//           <ul>
+//             <li><a href="#Introduccion">Introduccion</a></li>
+//             <li><a href="#Redes">Redes</a></li>
+//             <li><a href="#Proyectos">Proyectos</a></li>
+//           </ul>
+//         </article>
+//         <!-- Icono de la página -->
+//         <article>
+//           <img src="assets/imgs/icono.png" alt="" />
+//         </article>
+//       </section>
+//       <!-- creditos -->
+//       <address>agustinlazari594@gmail.com</address>
+//     </footer>
+//   </body>
+// </html>
+
+
---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <>
+        <header>
+            <nav>
+                <ul>
+                <li><a href="#Introduccion">Introduccion</a></li>
+                <li><a href="#Redes">Redes</a></li>
+                <li><a href="#Proyectos">habilidades</a></li>
+                <li><a href="#Proyectos">habilidades</a></li>
+                <li><a href="#Proyectos">Proyectos</a></li>
+                </ul>
+            </nav>
+        </header>
+    </>
+  )
+}
+
+export default Header
