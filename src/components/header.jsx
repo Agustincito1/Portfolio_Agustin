50894a3fb53265880d1fd9de62564d8a38d1145f
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-03 22:44:49 -0300

    header para celulares

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,20 @@
 function Header() {
   return (
     <>
-        <header className="bg-[var(--dv)] text-[var(--lv)]">
-            <nav className="w-full ">
-                <ul>
-                  <li><a href="#Agustin">Agustin</a></li>
-                  <li><a href="#Aboutme">Sobre mi</a></li>
-                  <li><a href="#Porfolio">Proyectos</a></li>
-                  <li><a href="#Skills">Habilidades</a></li>
-                  <li><a href="#Education">Educacion</a></li>
-                  <li><a href="#Contactme">Contacto</a></li>
+        <header className="w-full h-fit bg-[var(--dv)] text-[var(--lv)]  p-2">
+            <nav className="w-full h-fit">
+                <ul className="w-full h-full linkText flex text-center justify-around text-[3vw] ">
+                  <li className="w-fit h-fit"><a href="#Agustin" className="block transition delay-20 duration-200 ease-in-out hover:-translate-y-0.5 hover:text-[var(--am)] hover:underline underline-offset-3  ">Agustin</a></li>
+                  <li className="w-fit h-fit"><a href="#Aboutme" className="block transition delay-20 duration-200 ease-in-out hover:-translate-y-0.5 hover:text-[var(--am)] hover:underline underline-offset-3  ">Sobre mi</a></li>
+                  <li className="w-fit h-fit"><a href="#Porfolio" className="block transition delay-20 duration-200 ease-in-out hover:-translate-y-0.5 hover:text-[var(--am)] hover:underline underline-offset-3  ">Proyectos</a></li>
+                  <li className="w-fit h-fit"><a href="#Skills" className="block transition delay-20 duration-200 ease-in-out hover:-translate-y-0.5 hover:text-[var(--am)] hover:underline underline-offset-3  ">Habilidades</a></li>
+                  <li className="w-fit h-fit"><a href="#Education" className="block transition delay-20 duration-200 ease-in-out hover:-translate-y-0.5 hover:text-[var(--am)] hover:underline underline-offset-3  ">Educacion</a></li>
+                  <li className="w-fit h-fit"><a href="#Contactme" className="block transition delay-20 duration-200 ease-in-out hover:-translate-y-0.5 hover:text-[var(--am)] hover:underline underline-offset-3  ">Contacto</a></li>
                 </ul>
             </nav>
         </header>
     </>
   )
 }
-
+// border-red-500 border
 export default Header
