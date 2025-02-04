3493d448b596e7a3bc84a2b397367a88d0158ccc
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-04 10:58:19 -0300

    terminado la parte smartphones

--- src/components/Agustin/dowloadCV.jsx
+++ src/components/Agustin/dowloadCV.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function Cv({ text }) {
     return (
       <>
-        <button className='bg-[var(--gp)] font-bold text-[var(--bp)] p-[20px] rounded-sm border border-color-[var(--bp)]'>{text}</button>
+        <button className=' bg-[var(--gp)] w-fit h-fit button font-bold text-[var(--bp)] p-[4%] rounded-[2px] border-[0.5px] border-color-[var(--bp)]'>{text}</button>
       </>
     )
 }
--- src/pages/agustin.jsx
+++ src/pages/agustin.jsx
@@ -5,16 +5,16 @@ import React from 'react';
 function Agustin() {
     return (
       <>
-        <section className="grid  grid-cols-[0.7fr_1fr_1fr] gap-0 grid-rows-2 grid-rows-[35%_auto]  justify-items-center">
+        <section className="grid  grid-cols-[0.7fr_1fr_1fr] grid-rows-2 grid-rows-[65%_35%]  justify-items-center">
            
-            <article className="row-span-3">
+            <article className="row-span-3 ">
                 <Profile location="./src/imgs/profile.png" title="Imagen profile"/>
             </article>
             <article className="col-span-2 pt-2">
                 <h1 className="text-[var(--bh)] title">Agustin Lazaro Ardeola de Olivera</h1>
                 <h2 className="text-[var(--lv)] subtitle">Full-Stack Development</h2>
             </article>
-            <article>
+            <article className="w-full grid pl-[4%]">
                 <Cv text="Descargar mi CV"></Cv>
             </article>
             <article className="flex gap-1 justify-self-end h-fit mr-2">
