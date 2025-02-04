cc979e989f67610cdf8bbf68109415644933da97
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-04 08:12:15 -0300

    estilo header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <>
-        <header className="w-full h-fit bg-[var(--dv)] text-[var(--lv)]  p-2">
+        <header className="w-full h-fit bg-[var(--dv)] text-[var(--lv)]  p-[3%] fixed ">
             <nav className="w-full h-fit">
                 <ul className="w-full h-full linkText flex text-center justify-around underline-offset-[30%]">
                   <li className="w-fit h-fit "><a href="#Agustin" className="block transition delay-20 duration-200 ease-in-out hover:-translate-y-0.5 hover:text-[var(--am)] hover:underline   hover:scale-120">Agustin</a></li>
