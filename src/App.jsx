ad48c14da2a36bd1868afc29c254a5b774a5940c
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-05 09:44:00 -0300

    aboutme completado

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './components/footer.jsx'
 import Header from './components/header.jsx'
 import Agustin from './pages/agustin.jsx';
 import Portfolio from './pages/portfolio.jsx';
+import Aboutme from './pages/aboutme.jsx';
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
 
       <Header/>
       <Agustin/>
+      <Aboutme></Aboutme>
       {/* <Portfolio/> */}
       <Footer/>
 
---
+++ src/pages/aboutme.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+function Aboutme() {
+    return (
+      <>
+        <section className="max-w-[85vw] w-full grid gap-[3%]">
+            <h2 className="titleHard text-[var(--lv)]">Sobre mi</h2>
+            <p className="Text text-[var(--bh)]">
+                Soy un programador en constante evolucion, siempre en busca de desafios que me 
+                impulsen a alcanzar mis límites que me permita seguir aprendiendo y mejorando. 
+                Mi objetivo es crecer tanto profesional como técnicamente, y contribuir con sol
+                uciones innovadoras que resuelvan problemas reales.
+            </p>
+            <p className="Text text-[var(--lv)]">
+                Actualmente, estoy estudiando por mi cuenta para convertirme en desarrollador Full-Stack
+            </p>
+        </section>
+      </>
+    )
+}
+
+export default Aboutme
