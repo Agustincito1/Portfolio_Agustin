16bb793bb4452bad4aec09bde35aa985f576202b
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-05 09:33:48 -0300

    ahora si profile terminado

--- src/components/Agustin/profile.jsx
+++ src/components/Agustin/profile.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function Profile({ location, title }) {
   return (
     <div className='bg-[var(--bh)] rounded-full w-full p-[0.5vw]' >
-      <img className='rounded-full w-[20vw] h-[20vw]' src={location} alt={title} />
+      <img className='rounded-full w-[20vw] min-h-[20px] min-w-[20px] h-[19vw]' src={location} alt={title} />
     </div>
   )
 }
--- src/pages/agustin.jsx
+++ src/pages/agustin.jsx
@@ -5,19 +5,19 @@ import React from 'react';
 function Agustin() {
     return (
       <>
-        <section className="grid  grid-cols-[0.7fr_1fr_1fr] grid-rows-2 grid-rows-[65%_35%]  justify-items-center">
+        <section className=" grid max-w-[85vw] items-center min-h-[5px] grid-cols-[0.6fr_1fr_1fr] grid-rows-2 grid-rows-[2fr_1fr]  ">
            
             <article className="row-span-3 ">
                 <Profile location="./src/imgs/profile.png" title="Imagen profile"/>
             </article>
-            <article className="col-span-2 pt-2">
+            <article className="col-span-2 pt-2 justify-self-end">
                 <h1 className="text-[var(--bh)] title">Agustin Lazaro Ardeola de Olivera</h1>
                 <h2 className="text-[var(--lv)] subtitle">Full-Stack Development</h2>
             </article>
-            <article className="w-full grid pl-[4%]">
+            <article className="w-full grid pl-[4%] ml-[5%]">
                 <Cv  text="Descargar mi CV"></Cv>
             </article>
-            <article className="flex gap-1 justify-self-end h-fit mr-2">
+            <article className="flex gap-[5%]  h-fit justify-end">
                 <div className="bg-[var(--bh)] w-[4vw] h-[4vw] rounded-full"></div>
                 <div className="bg-[var(--ls)] w-[4vw] h-[4vw] rounded-full"></div>
                 <div className="bg-[var(--mi)] w-[4vw] h-[4vw] rounded-full"></div>
