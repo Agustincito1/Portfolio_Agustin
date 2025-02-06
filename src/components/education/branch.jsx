8f84f6d766954792dc1fe0511f17ff8e2790a6b4
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-06 10:25:23 -0300

    intentando crear una buena educacion

--- src/components/education/branch.jsx
+++ src/components/education/branch.jsx
@@ -1,21 +1,37 @@
-// import PropTypes from 'prop-types'; 
-// function Branch({ year, colorR, arrayText, colorRtext }) {
+import React, { useState } from "react";
+
+
 function Branch() {
-  return (
+    const [isHovered, setIsHovered] = useState(false);
+
+    return (
         <>
-            <div className="h-60">
-                <p className="title text-[var(--bh)] flex gap-2">2021<div className="bg-[var(--bh)] rounded-full w-2 h-2"></div></p>
+            <div className="h-60 ">
+                <div 
+                    className="padre bg-[var(--bh)] rounded-full w-2 h-2 m-auto relative"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
+                    <p
+                        className={`title  ml-4  text-[var(--bh)]  hijo ${isHovered ? 'hovered' : 'hidden'}`}
+                    >2021</p>
+                    
+                </div>
                 
-                <ul className="">
-                    <li>Logica y programacion</li>
-                    <li>Pseint</li>
-                    <li>CMD</li>
-                </ul>
+
+
+
+                {/* <ul className="bg-red-100 w-2 h-50 m-auto">
+                    <li className="absolute mt-[0%] w-16 h-1 bg-red-100 ml-[0%] Text relative">
+                        <div className="w-2 h-2 rounded-full bg-black absolute left-17"></div>
+                        <p className="absolute left-20 w-16 ">Logica y programacion</p>
+                    </li>
+                </ul> */}
             </div>
         
         </>
-    
-  )
+        
+    )
 }
 
 
