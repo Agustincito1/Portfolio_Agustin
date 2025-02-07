0592c509af4f66b8bbd96de076c2bfa47d03efac
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-07 10:06:28 -0300

    creacion de un separador de secciones

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Projects from './pages/projects.jsx';
 import Skill from './pages/skills.jsx';
 import Education from './pages/education.jsx';
 import ScrollReveal from './functions/ScrollReveal.jsx';
-
+import Separate from './components/separador.jsx';
 
 const App = () => {
   return (
@@ -20,18 +20,23 @@ const App = () => {
       <ScrollReveal>
         <Agustin id="Agustin"/>
       </ScrollReveal>
+      <Separate></Separate>
       <ScrollReveal>
         <Aboutme id="Aboutme"/>
       </ScrollReveal>
+      <Separate></Separate>
       <ScrollReveal>
         <Projects id="Projects"/>
       </ScrollReveal>
+      <Separate></Separate>
       <ScrollReveal>
         <Skill id="Skills"/>
       </ScrollReveal>
+      <Separate></Separate>
       <ScrollReveal>
         <Education id="Education"/>
       </ScrollReveal>
+      <Separate></Separate>
       <ScrollReveal>
         <Footer id="Contactme"/>
       </ScrollReveal>
---
+++ src/components/separador.jsx
@@ -0,0 +1,12 @@
+function Separate(){
+    return(
+        <>
+        
+            <hr className="bg-[var(--bh)] text-[var(--bh)] w-[102%] h-[200%]"/>
+        
+        </>
+    )
+   
+}
+
+export default Separate
