7a48170e11f4eef0007c9d5d8741763318bc48ef
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-08 09:12:03 -0300

    modificando el apartado projects

--- src/components/projects/cardProject.jsx
+++ src/components/projects/cardProject.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types'; 
 function CardProject({ text, title, img, arrayLeng }) {
   return (
-        <div className='bg-[var(--lv)] min-h-[40px] grid grid-rows-[50%_50%] h-fit p-[1%]'>
+        <div className='bg-[var(--bh)] min-h-[40px]  grid grid-rows-[50%_50%] h-[30vw] p-[1%]'>
             <section>
                 <img className='h-full' src={img} alt="" />
             </section>
-            <section className='grid grid-rows-[1fr_1fr] p-[3%]'>
+            <section className='grid grid-rows-[1fr_1fr_1fr] p-[3%] gap-[8%]'>
                 <article >
                     <h3 className='Text'>{title}</h3>
                     <p  className='Textp'>{text}</p>
@@ -16,6 +16,9 @@ function CardProject({ text, title, img, arrayLeng }) {
                             <img className="" key={index} src={image} />
                         ))
                     } 
+                </article>
+                <article className="grid grid-cols-2 items-center w-full ">
+                    <img className="w-[3vw] m-auto" src="./imgs/github.png" alt="" />
                 </article>
             </section>
         </div>
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -11,7 +11,9 @@ function Projects() {
               <h2 className="titleHard text-[var(--lv)]">Porfolio</h2>
               <article className="grid grid-cols-4 h-[100%]">
                 <CardProject text="creado para hacer esto" title="Porfolio" img="/imgs/porfolio.jpeg" arrayLeng={array}></CardProject>
+                
               </article>
+              
           </section>
         </ScrollReveal>
       </>
