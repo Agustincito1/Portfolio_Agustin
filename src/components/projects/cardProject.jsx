c0f830bbd144a1fe243a99f776f3b3a9dab29bb6
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-08 23:38:52 -0300

    mejorando estilo de projects

--- src/components/projects/cardProject.jsx
+++ src/components/projects/cardProject.jsx
@@ -1,24 +1,24 @@
 import PropTypes from 'prop-types'; 
-function CardProject({ text, title, img, arrayLeng }) {
+function CardProject({ text, title, img, arrayLeng, linkGit }) {
   return (
-        <div className='bg-[var(--bh)] min-h-[40px]  grid grid-rows-[50%_50%] h-[30vw] p-[1%]'>
+        <div className='shadowProjects bg-[var(--bh)] min-h-[40px] rounded-[0.4vw] grid grid-rows-[40%_50%] h-[25vw] p-[1%]  hover:scale-110 duration-100'>
             <section>
-                <img className='h-full' src={img} alt="" />
+                <img className=' h-full rounded-[0.4vw] p-[1%]' src={img} alt="" />
             </section>
-            <section className='grid grid-rows-[1fr_1fr_1fr] p-[3%] gap-[8%]'>
-                <article >
+            <section className='grid grid-rows-[55%_35%_10%] p-[1%] gap-[0%]'>
+                <article className='' >
                     <h3 className='Text'>{title}</h3>
-                    <p  className='Textp'>{text}</p>
+                    <p  className='Textproject'>{text}</p>
                 </article>
-                <article className="grid grid-cols-6 items-center w-full ">
+                <article className="flex m-auto justify-center w-full ">
                     {
                         arrayLeng.map((image, index) => (
-                            <img className="" key={index} src={image} />
+                            <img className="w-[4vw]" key={index} src={image} />
                         ))
                     } 
                 </article>
-                <article className="grid grid-cols-2 items-center w-full ">
-                    <img className="w-[3vw] m-auto" src="./imgs/github.png" alt="" />
+                <article className="flex justify-center w-full ">
+                    <a href={linkGit} className="w-fit" target='_blank'><img className="w-[3vw] m-auto" src="./imgs/github.png" alt="" /></a>
                 </article>
             </section>
         </div>
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CardProject from '../components/projects/CardProject';
 import ScrollReveal from '../functions/ScrollReveal.jsx';
-const array = ["/imgs/js.png"]
+const Project1 = ["/imgs/js2.svg", "/imgs/tailwind-css.svg", "/imgs/react.svg"]
 
 function Projects() {
     return (
@@ -10,7 +10,7 @@ function Projects() {
           <section className="mb-[15%] mt-[10%] max-w-[85vw] w-full  grid gap-[3%] font-bold">
               <h2 className="titleHard text-[var(--lv)]">Porfolio</h2>
               <article className="grid grid-cols-4 h-[100%]">
-                <CardProject text="creado para hacer esto" title="Porfolio" img="/imgs/porfolio.jpeg" arrayLeng={array}></CardProject>
+                <CardProject text="creado para hacer esto" title="Porfolio" img="/imgs/porfolio.jpeg" linkGit="git@github.com:Agustincito1/portfolio-2.0.git" arrayLeng={Project1}></CardProject>
                 
               </article>
               
