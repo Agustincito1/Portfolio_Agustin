58877d0c72a79be6ecc8ac19dc60087f8fc156ec
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-09 09:51:29 -0300

    actualizando modulos

--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,8 @@ import App from "./App";
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+);
