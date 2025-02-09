4220d9e80df354002526a80464e95bf3ee27a7c9
AgustinArdeola <agustinlazari594@gmail.com>
2025-02-09 08:25:38 -0300

    añadiendo link de github al proyecto

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -10,7 +10,7 @@ function Projects() {
           <section className="mb-[15%] mt-[10%] max-w-[85vw] w-full  grid gap-[3%] font-bold">
               <h2 className="titleHard text-[var(--lv)]">Porfolio</h2>
               <article className="grid grid-cols-4 h-[100%]">
-                <CardProject text="creado para hacer esto" title="Porfolio" img="/imgs/porfolio.jpeg" linkGit="git@github.com:Agustincito1/portfolio-2.0.git" arrayLeng={Project1}></CardProject>
+                <CardProject text="creado para hacer esto" title="Porfolio" img="/imgs/porfolio.jpeg" linkGit="https://github.com/Agustincito1/portfolio-2.0.git" arrayLeng={Project1}></CardProject>
                 
               </article>
               
